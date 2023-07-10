d7937d545736b544ed324aed098bbbf7f5c9ca24
Jays-PC <jaysqvl@gmail.com>
2023-07-09 23:43:27 -0700

    Send 1000 Hello's added

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -15,14 +15,45 @@ public class TCPClient {
         BufferedReader in = new BufferedReader(new InputStreamReader(is));
 
         // send data
-        long start = System.currentTimeMillis();  // Start time
+        // Client's ACK receive time - start = RTT
+        // Hello is already constructed so the creation of the packet is not included
+        long start = System.currentTimeMillis();
         out.println("hello");
 
         // receive response
         String response = in.readLine();
         long end = System.currentTimeMillis();  // End time
-        System.out.println("Server response: " + response);
-        System.out.println("Round-trip time: " + (end - start) + " ms");
+        long RTT = end - start;
+
+        System.out.println(response);
+        System.out.println("Round-trip time (RTT): " + RTT + " ms");
+
+        // send 1000 "hello" data messages
+        long minRTT = RTT;
+        long maxRTT = RTT;
+        long totalRTT = RTT;
+        for (int i = 0; i < 999; i++) {
+            // send data
+            start = System.currentTimeMillis();  // Start time
+            out.println("hello");
+
+            // receive response
+            response = in.readLine();
+            end = System.currentTimeMillis();  // End time
+            RTT = end - start;
+
+            // update RTT stats
+            minRTT = Math.min(minRTT, RTT);
+            maxRTT = Math.max(maxRTT, RTT);
+            totalRTT += RTT;
+        }
+
+        // print statistics
+        System.out.println("\nTCP averages:");
+        System.out.println("Min RTT: " + minRTT + " ms");
+        System.out.println("Max RTT: " + maxRTT + " ms");
+        System.out.println("Avg RTT: " + (totalRTT / 1000.0) + " ms");
+
 
         // close resources
         out.close();
